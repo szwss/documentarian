e7ac1a3ef877aec95bad56f27b29c39ad0c7568d
vagrant <vagrant@homestead>
2018-12-30 12:21:13 +0000

    修改静态资源css/js/image等局域网共享访问时无法访问的bug,index.blade.php中加上资源链接的前缀

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <title>{{$page['title']}}</title>
 
-    <link rel="stylesheet" href="css/style.css" />
-    <script src="js/all.js"></script>
+    <link rel="stylesheet" href="{{ config('apidoc.static_resource_url',null) }}css/style.css" />
+    <script src="{{ config('apidoc.static_resource_url',null) }}js/all.js"></script>
 
 
     @if(isset($page['language_tabs']))
@@ -23,11 +23,11 @@
     <a href="#" id="nav-button">
       <span>
         NAV
-        <img src="images/navbar.png" />
+        <img src="{{ config('apidoc.static_resource_url',null) }}images/navbar.png" />
       </span>
     </a>
     <div class="tocify-wrapper">
-        <img src="images/logo.png" />
+        <img src="{{ config('apidoc.static_resource_url',null) }}images/logo.png" />
         @if(isset($page['language_tabs']))
             <div class="lang-selector">
                 @foreach($page['language_tabs'] as $lang)
